af2dd824239afa00921c6b92d0873f403e5c3407
Tiffany Ho <Tiffanyho0903@gmail.com>
2024-09-11 13:00:01 -0400

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,19 @@
 //student
+public class Student
+{
+    private String firstName;
+    private String lastName;
+    private int gradeLevel;
+    
+    public Student(String fName, String lName, int grade)
+    {
+        firstName = fName;
+        lastName = lName;
+        gradeLevel = grade;
+    }
+    
+    public String toString()
+    {
+        return firstName + " " + lastName + " is in grade: " + gradeLevel;
+    }
+}
